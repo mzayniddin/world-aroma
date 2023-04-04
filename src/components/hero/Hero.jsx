f25af5166b2f42ba742e035329a021fade925742
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-04 15:06:38 +0500

    finished hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,17 @@ import "./Hero.scss";
 const Hero = () => {
     return (
         <section className="hero">
-            <h1>This is hero</h1>
+            <div className="container">
+                <div className="hero-content">
+                    <span className="hero__sub-title">
+                        Fresh Flower & Gift Shop
+                    </span>
+                    <h1 className="hero-title">
+                        Making beautiful flowers a part of your life.
+                    </h1>
+                    <button className="hero__btn-shop">Shop now</button>
+                </div>
+            </div>
         </section>
     );
 };
