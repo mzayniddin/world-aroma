67dab135b9c359b170c4af4c7de39e6421e037b4
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-05 15:35:58 +0500

    changed hero to swiper

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,42 @@
+// HOOKS
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper";
+
+// SCSS
+import "swiper/css";
+import "swiper/css/effect-fade";
 import "./Hero.scss";
+
+// IMAGES
+import image1 from "../../assets/images/hero-img1.jpg";
+import image2 from "../../assets/images/hero-img2.jpg";
 
 const Hero = () => {
     const { t } = useTranslation();
+
     return (
         <section className="hero">
+            <Swiper
+                slidesPerView={1}
+                loop={true}
+                effect={"fade"}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                speed={2500}
+                modules={[Autoplay, EffectFade]}
+                className="hero__swiper"
+            >
+                <SwiperSlide>
+                    <img className="hero__swiper-img" src={image1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className="hero__swiper-img" src={image2} alt="" />
+                </SwiperSlide>
+            </Swiper>
             <div className="container">
                 <div className="hero-content">
                     <span className="hero__sub-title">
