3f32c36b108d341a45f37685a3d9f7ac2e2e250b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-05 23:19:14 +0500

    added subhero images

--- src/components/sub-hero/SubHero.jsx
+++ src/components/sub-hero/SubHero.jsx
@@ -8,11 +8,17 @@ import aImg2 from "../../assets/ui-icons/advantage-img2.svg";
 import aImg3 from "../../assets/ui-icons/advantage-img3.svg";
 import aImg4 from "../../assets/ui-icons/advantage-img4.svg";
 
+import sImg1 from "../../assets/images/sub-hero-img1.jpg";
+import sImg2 from "../../assets/images/sub-hero-img2.jpg";
+import sImg3 from "../../assets/images/sub-hero-img3.jpg";
+import sImg4 from "../../assets/images/sub-hero-img4.jpg";
+
 // SCSS
 import "./SubHero.scss";
 
 const SubHero = () => {
     const aImages = [aImg1, aImg2, aImg3, aImg4];
+    const subHeroImages = [sImg1, sImg2, sImg4, sImg3];
     return (
         <div className="sub-hero">
             <div className="container">
@@ -26,7 +32,11 @@ const SubHero = () => {
                     ))}
                 </div>
                 <div className="sub-hero__images-wrapper">
-                    
+                    {subHeroImages.map((image) => (
+                        <div className="sub-hero__img-wrap">
+                            <img src={image} alt="Flowers" />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
