d14c99f86f635c4a4c0109b84938ef2714a9fee4
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-07 10:37:29 +0500

    footer contact styled

--- src/components/footer/footer-contact/FooterContact.jsx
+++ src/components/footer/footer-contact/FooterContact.jsx
@@ -1,12 +1,21 @@
 import "./FooterContact.scss";
+import { FiPhone } from "react-icons/fi";
 
 const FooterContact = () => {
     return (
         <div>
             <strong className="footer__list-title">Contact</strong>
             <ul className="footer__contact footer__list">
-                <li className="footer__list-item">Tell: 525624553474</li>
-                <li className="footer__list-item">Tell: 525624553474</li>
+                <li className="footer__list-item">
+                    <a href="tel:+998977993433">
+                        <FiPhone /> +998977993433
+                    </a>
+                </li>
+                <li className="footer__list-item">
+                    <a href="tel:+998977993433">
+                        <FiPhone /> +998977993433
+                    </a>
+                </li>
             </ul>
         </div>
     );
--- src/components/footer/footer-nav/FooterNav.jsx
+++ src/components/footer/footer-nav/FooterNav.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import "./FooterNav.scss";
 
 const FooterNav = () => {
     const { t } = useTranslation();
