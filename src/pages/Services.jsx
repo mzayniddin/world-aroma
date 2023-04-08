52af3f8fde1836b2e324f418418cd72106fc4c14
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-08 11:31:00 +0500

    services page was finished

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,15 +1,21 @@
 // COMPONENTS
-
 import Intro from "../components/intro/Intro";
 import OurServices from "../components/our-services/OurServices";
 import ServicesPrice from "../components/services-price/ServicesPrice";
+import AdsContact from "../components/ads-contact/AdsContact";
+import AdsInsta from "../components/ads-insta/AdsInsta";
+
+// IMAGE
+import bgAds from "../assets/images/service-ads-bg.jpg";
 
 const Services = () => {
     return (
         <main>
             <Intro title={"services"} />
-            <OurServices/>
-            <ServicesPrice/>
+            <OurServices />
+            <ServicesPrice />
+            <AdsContact bg={bgAds} />
+            <AdsInsta />
         </main>
     );
 };
