f37e819f399460b8293bf36cbf09cb0299776dc0
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-04-12 00:27:17 +0500

    added login modal

---
+++ src/components/login-modal/LoginModal.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import "./LoginModal.scss";
+
+const LoginModal = ({ setIsLogged, isLogged }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const possibleError = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (
+            username === import.meta.env.VITE_USERNAME &&
+            password === import.meta.env.VITE_PASSWORD
+        ) {
+            setIsLogged(true);
+        }
+        possibleError.current.textContent = "* Incorrect username or password";
+    };
+
+    return (
+        <div
+            style={isLogged ? { display: "none" } : { display: "flex" }}
+            className="login-modal"
+        >
+            <div className="login-modal__card">
+                <form onSubmit={handleSubmit} className="login-modal__form">
+                    <div className="login-modal__input-wrap">
+                        <label htmlFor="admin-username">Username:</label>
+                        <input
+                            onChange={(e) => setUsername(e.target.value)}
+                            id="admin-username"
+                            type="text"
+                        />
+                    </div>
+                    <div className="login-modal__input-wrap">
+                        <label htmlFor="admin-password">Password:</label>
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                            className=""
+                            id="admin-password"
+                            type="password"
+                        />
+                    </div>
+                    <button className="login-modal__submit-btn">Submit</button>
+                    <p
+                        className="login-modal__error-msg"
+                        ref={possibleError}
+                    ></p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginModal;
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import AdminTable from "../components/admin-table/AdminTable";
+import LoginModal from "../components/login-modal/LoginModal";
 
 const Admin = () => {
+    const [isLogged, setIsLogged] = useState(false);
+
     return (
         <main>
-            <AdminTable />
+            <LoginModal isLogged={isLogged} setIsLogged={setIsLogged} />
+            {isLogged ? <AdminTable /> : <></>}
         </main>
     );
 };
